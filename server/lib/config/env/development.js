fe8e487eb4e99732da66a9b86a3f53f25c8d29e4
Olivier <devSamsane@users.noreply.github.com>
2017-09-28 11:21:22 +0200

    feat(logs): Suppression des paramètres du logger de l'env dev

--- server/lib/config/env/development.js
+++ server/lib/config/env/development.js
@@ -8,15 +8,5 @@ module.exports = {
   port: process.env.PORT || 3000,
   host: process.env.HOST || '0.0.0.0',
   domain: process.env.DOMAIN || 'http://localhost',
-  livereload: true,
-  log: {
-    format: 'dev',
-    fileLogger: {
-      directoryPath: process.cwd(),
-      fileName: 'app.log',
-      maxsize: 10485760,
-      maxFiles: 2,
-      json: false
-    }
-  }
+  livereload: true
 };
