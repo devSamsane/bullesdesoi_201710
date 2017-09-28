6292d6af837ec248b70ee2988060e91e1eecb1e7
Olivier <devangularjs@gmail.com>
2017-09-28 23:26:55 +0200

    fix(ApiError): lint issues

--- server/lib/helpers/ApiError.js
+++ server/lib/helpers/ApiError.js
@@ -6,22 +6,22 @@
 
 const API_ERROR_CODES = {
   serverError: 'SERVER_ERROR'
-}
+};
 
 class ApiError extends Error {
   constructor (message, {status, code} = {}) {
-    super(message)
+    super(message);
 
     // Configuration du code status HTTP
-    this.status = status || 500
+    this.status = status || 500;
 
     // Configuration du code erreur api
-    this.code = code || API_ERROR_CODES.serverError
+    this.code = code || API_ERROR_CODES.serverError;
 
     // Assurance de l'utilisation du nom de la subClass
-    this.name = this.constructor.name
+    this.name = this.constructor.name;
 
-    Error.captureStackTrace(this, this.constructor)
+    Error.captureStackTrace(this, this.constructor);
   }
 }
 
