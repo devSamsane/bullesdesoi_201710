aa614e88d0e1db5fd564f080af3aa9364e215385
Olivier <devSamsane@users.noreply.github.com>
2017-09-28 11:20:32 +0200

    feat(logs): Ajout des paramètres d'env. du logger

--- server/lib/config/env/default.js
+++ server/lib/config/env/default.js
@@ -3,5 +3,15 @@ module.exports = {
     title: 'Bulles de Soi',
     description: 'Site web de sophrologie et prise de rendez-vous en ligne',
     keywords: 'sophrologie, relaxation, gestion du stress, perinatalité, enfance, adolescence'
+  },
+  log: {
+    //format: process.env.LOG_DIR_PATH || 'dev',
+    fileLogger: {
+      directoryPath: process.env.LOG_DIR_PATH || process.cwd(),
+      fileName: process.env.LOG_FILE || 'app.log',
+      maxsize: 10485760,
+      maxFiles: 2,
+      json: false
+    }
   }
 };
