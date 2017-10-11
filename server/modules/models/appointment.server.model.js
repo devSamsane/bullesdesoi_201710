53e9a41f7bfe853edbd6bdca78e20c93c17f5319
Olivier <devSamsane@users.noreply.github.com>
2017-10-11 17:07:33 +0200

    feat(validation): mise à jour appointment

--- server/modules/models/appointment.server.model.js
+++ server/modules/models/appointment.server.model.js
@@ -1,10 +1,12 @@
 // Déclaration des librairies nodeJS
+const path = require('path');
 
 // Déclaration des librairies
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 // Déclaration des fichiers de configuration
+const config = require(path.resolve('./server/lib/config/index'));
 
 /**
  * Paramétrage du schéma `Appointment`
@@ -16,11 +18,11 @@ const AppointmentSchema = new Schema({
   },
   startDateTime: {
     type: Date,
-    required: true
+    required: [true, config.db.msg.global.required]
   },
   endDateTime: {
     type: Date,
-    required: true
+    required: [true, config.db.msg.global.required]
   },
   isConfirmed: {
     type: Boolean,
