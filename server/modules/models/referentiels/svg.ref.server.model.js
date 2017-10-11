bbe45dd59888f148c6a76cb081622db6f0a1cac5
Olivier <devSamsane@users.noreply.github.com>
2017-10-11 17:05:09 +0200

    feat(model): création du référentiel svg

---
+++ server/modules/models/referentiels/svg.ref.server.model.js
@@ -0,0 +1,30 @@
+// Déclaration des librairies nodeJS
+const path = require('path');
+
+// Déclaration des librairies
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Déclaration des fichiers de configuration
+const config = require(path.resolve('./server/lib/config/index'));
+
+/**
+ * Paramétrage du schema `RefSVGSchema`
+ */
+const RefSVGSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, config.db.msg.global.required]
+  },
+  content: {
+    type: String,
+    required: [true, config.db.msg.global.required]
+  }
+
+});
+
+/**
+ * Création du model `RefSVG`
+ * @name RefSVG
+ */
+mongoose.model('RefSVG', RefSVGSchema);
